76274540f97e73559ccea4799c42ba802f6a8bf2
Tim Veil <tjveil@gmail.com>
2018-09-27 18:33:55 -0400

    updating run.sh

--- run.sh
+++ run.sh
@@ -18,7 +18,7 @@ hadoop fs -copyFromLocal ${TEST_DATA_DIR}/map-test.csv /tmp/map_test
 hadoop fs -mkdir /tmp/struct_test
 hadoop fs -copyFromLocal ${TEST_DATA_DIR}/struct-test.csv /tmp/struct_test
 
-
+${HIVE_HOME}/bin/beeline -u jdbc:hive2://localhost:10000/default -e 'create database if not exists tests'
 ${HIVE_HOME}/bin/beeline -u jdbc:hive2://localhost:10000/tests -f ${TEST_DATA_DIR}/array-test.sql
 ${HIVE_HOME}/bin/beeline -u jdbc:hive2://localhost:10000/tests -f ${TEST_DATA_DIR}/data-type-test.sql
 ${HIVE_HOME}/bin/beeline -u jdbc:hive2://localhost:10000/tests -f ${TEST_DATA_DIR}/date-time-test.sql
