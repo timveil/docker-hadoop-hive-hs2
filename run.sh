75876b5df96bfa6773113c8c76114a5532ce98c8
Tim Veil <tjveil@gmail.com>
2018-09-27 19:08:59 -0400

    updating run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 ${HIVE_HOME}/bin/hiveserver2 --hiveconf hive.server2.enable.doAs=false
 
-
+echo "adding files to hadoop"
 hadoop fs -mkdir /tmp/array_test
 hadoop fs -copyFromLocal ${TEST_DATA_DIR}/array-test.csv /tmp/array_test
 
@@ -18,9 +18,14 @@ hadoop fs -copyFromLocal ${TEST_DATA_DIR}/map-test.csv /tmp/map_test
 hadoop fs -mkdir /tmp/struct_test
 hadoop fs -copyFromLocal ${TEST_DATA_DIR}/struct-test.csv /tmp/struct_test
 
+echo "creating database"
 ${HIVE_HOME}/bin/beeline -u jdbc:hive2://localhost:10000/default -e 'create database if not exists tests'
+
+echo "running sql statements"
 ${HIVE_HOME}/bin/beeline -u jdbc:hive2://localhost:10000/tests -f ${TEST_DATA_DIR}/array-test.sql
 ${HIVE_HOME}/bin/beeline -u jdbc:hive2://localhost:10000/tests -f ${TEST_DATA_DIR}/data-type-test.sql
 ${HIVE_HOME}/bin/beeline -u jdbc:hive2://localhost:10000/tests -f ${TEST_DATA_DIR}/date-time-test.sql
 ${HIVE_HOME}/bin/beeline -u jdbc:hive2://localhost:10000/tests -f ${TEST_DATA_DIR}/map-test.sql
 ${HIVE_HOME}/bin/beeline -u jdbc:hive2://localhost:10000/tests -f ${TEST_DATA_DIR}/struct-test.sql
+
+echo "all finished with sql statements"
