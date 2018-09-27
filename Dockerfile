82680262915e10a689a9a4dd37e4f09dd6221ba7
Tim Veil <tjveil@gmail.com>
2018-09-27 18:24:06 -0400

    trying to add some test data

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,21 @@ FROM timveil/docker-hadoop-hive-base:3.1.x
 
 LABEL maintainer="tjveil@gmail.com"
 
+ENV TEST_DATA_DIR=/opt/test-data
+
 ADD run.sh /run.sh
-RUN chmod a+x /run.sh
+RUN chmod a+x /run.sh \
+    && mkdir -pv $TEST_DATA_DIR
+
+ADD data/array-test.csv $TEST_DATA_DIR
+ADD data/array-test.sql $TEST_DATA_DIR
+ADD data/data-type-test.csv $TEST_DATA_DIR
+ADD data/data-type-test.sql $TEST_DATA_DIR
+ADD data/date-time-test.csv $TEST_DATA_DIR
+ADD data/date-time-test.sql $TEST_DATA_DIR
+ADD data/map-test.csv $TEST_DATA_DIR
+ADD data/map-test.sql $TEST_DATA_DIR
+ADD data/struct-test.csv $TEST_DATA_DIR
+ADD data/struct-test.sql $TEST_DATA_DIR
 
 CMD ["/run.sh"]
--- run.sh
+++ run.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
 ${HIVE_HOME}/bin/hiveserver2 --hiveconf hive.server2.enable.doAs=false
+
+
+hadoop fs -mkdir /tmp/array_test
+hadoop fs -copyFromLocal ${TEST_DATA_DIR}/array-test.csv /tmp/array_test
+
+hadoop fs -mkdir /tmp/data_type_test
+hadoop fs -copyFromLocal ${TEST_DATA_DIR}/data-type-test.csv /tmp/data_type_test
+
+hadoop fs -mkdir /tmp/date_time_test
+hadoop fs -copyFromLocal ${TEST_DATA_DIR}/date-time-test.csv /tmp/date_time_test
+
+hadoop fs -mkdir /tmp/map_test
+hadoop fs -copyFromLocal ${TEST_DATA_DIR}/map-test.csv /tmp/map_test
+
+hadoop fs -mkdir /tmp/struct_test
+hadoop fs -copyFromLocal ${TEST_DATA_DIR}/struct-test.csv /tmp/struct_test
+
+
+${HIVE_HOME}/bin/beeline -u jdbc:hive2://localhost:10000/tests -f ${TEST_DATA_DIR}/array-test.sql
+${HIVE_HOME}/bin/beeline -u jdbc:hive2://localhost:10000/tests -f ${TEST_DATA_DIR}/data-type-test.sql
+${HIVE_HOME}/bin/beeline -u jdbc:hive2://localhost:10000/tests -f ${TEST_DATA_DIR}/date-time-test.sql
+${HIVE_HOME}/bin/beeline -u jdbc:hive2://localhost:10000/tests -f ${TEST_DATA_DIR}/map-test.sql
+${HIVE_HOME}/bin/beeline -u jdbc:hive2://localhost:10000/tests -f ${TEST_DATA_DIR}/struct-test.sql
