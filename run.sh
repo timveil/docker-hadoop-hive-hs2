45a4260f7ad0c744c11c64084240a00fc16c5c54
Tim Veil <tjveil@gmail.com>
2018-10-05 14:24:50 -0400

    updating classpath

--- run.sh
+++ run.sh
@@ -4,5 +4,9 @@ echo "adding tez libs hadoop"
 hadoop fs -mkdir -p /apps/tez
 hadoop fs -copyFromLocal ${TEZ_LIB_DIR}/*.jar /apps/tez/
 
+echo "update hadoop classpath for hiveserver2"
+export HADOOP_CLASSPATH=${TEZ_LIB_DIR}/*:${HADOOP_CLASSPATH};
+echo "hadoop classpath... ${HADOOP_CLASSPATH}"
+
 echo "starting hiveserver2"
 ${HIVE_HOME}/bin/hiveserver2 --hiveconf hive.server2.enable.doAs=false
