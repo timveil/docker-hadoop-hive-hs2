57445c72710df678861e5eb6ba4ed3437f911fca
Tim Veil <tjveil@gmail.com>
2018-10-05 13:33:44 -0400

    fixing tez; putting jars instead of budle

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 echo "adding tez libs hadoop"
 hadoop fs -mkdir -p /apps/tez
-hadoop fs -copyFromLocal $TEZ_LIB_DIR/*.jar /apps/tez/
+hadoop fs -copyFromLocal ${TEZ_LIB_DIR}/*.jar /apps/tez/
 
 echo "starting hiveserver2"
 ${HIVE_HOME}/bin/hiveserver2 --hiveconf hive.server2.enable.doAs=false
