5d4e261a9bad217e973a649ab2165ddee106a090
Tim Veil <tjveil@gmail.com>
2018-11-20 10:52:55 -0500

    updating echo

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ echo "adding tez libs hadoop"
 hadoop fs -mkdir -p /apps/tez
 hadoop fs -copyFromLocal /tmp/tez-minimal.tar.gz /apps/tez/
 
-echo "starting hiveserver2"
+echo "starting HiveServer2!"
 ${HIVE_HOME}/bin/hiveserver2 --hiveconf hive.server2.enable.doAs=false
