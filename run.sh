c980668d0b7436de5d24aef0cf905a8618167764
Tim Veil <tjveil@gmail.com>
2018-11-20 11:15:00 -0500

    updating for tez1

--- run.sh
+++ run.sh
@@ -2,7 +2,11 @@
 
 echo "adding tez libs hadoop"
 hadoop fs -mkdir -p /apps/tez
-hadoop fs -copyFromLocal /tmp/tez-minimal.tar.gz /apps/tez/
+hadoop fs -copyFromLocal ${TEZ_LIB_DIR}/*.jar /apps/tez/
+
+echo "update hadoop classpath for hiveserver2"
+export HADOOP_CLASSPATH=${TEZ_LIB_DIR}/*:${HADOOP_CLASSPATH};
+echo "hadoop classpath... ${HADOOP_CLASSPATH}"
 
 echo "starting HiveServer2!"
 ${HIVE_HOME}/bin/hiveserver2 --hiveconf hive.server2.enable.doAs=false
