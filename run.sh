b9027a745d0a0f0b87824776b4ac8b85425c8f7a
Tim Veil <tjveil@gmail.com>
2018-11-20 10:53:22 -0500

    updating echo

--- run.sh
+++ run.sh
@@ -8,5 +8,5 @@ echo "update hadoop classpath for hiveserver2"
 export HADOOP_CLASSPATH=${TEZ_LIB_DIR}/*:${HADOOP_CLASSPATH};
 echo "hadoop classpath... ${HADOOP_CLASSPATH}"
 
-echo "starting hiveserver2"
+echo "starting HiveServer2!"
 ${HIVE_HOME}/bin/hiveserver2 --hiveconf hive.server2.enable.doAs=false
